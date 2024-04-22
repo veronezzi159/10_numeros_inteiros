65625540067e629e2c02824874f1b17c33e25aa2
veronezzi159 <lucasveronezzi3@gmail.com>
2024-04-22 14:23:49 -0300

    Primeiro commit

--- 10_numeros_inteiros/Program.cs
+++ 10_numeros_inteiros/Program.cs
@@ -1,2 +1,44 @@
 ﻿int[] numeros = new int[10];
+int[] pares = new int[10];
+int[] impares = new int[10];
 
+Console.WriteLine("Digite 10 numeros inteiros");
+
+for (int i = 0; i < 10 ; i++)
+{
+    Console.WriteLine($"{i+1}º valor:");
+    numeros[i] = int.Parse(Console.ReadLine());
+
+    for (int j = i; j == i; j++)
+    {
+        if (numeros[i] % 2 == 0)
+        {
+            pares[j] = numeros[i];
+        }
+        else
+        {
+            impares[j] = numeros[i];
+        }
+    }
+}
+Console.Write("Pares: ");
+for (int i = 0; i < 10; i++)
+{
+    if (pares[i] != 0)
+    {
+        Console.Write($"{pares[i]},");
+    }
+}
+
+Console.Write("\nimpares: ");
+for (int i = 0; i < 10; i++)
+{
+    if (impares[i] != 0)
+    {
+        Console.Write($"{impares[i]}, ");
+    }
+}
+
+Console.WriteLine("\nPrimeira opção: " + numeros[0] + " Ultima opção: " + numeros[9]);
+
+    
